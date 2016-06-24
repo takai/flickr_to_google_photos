61d81742d9f3c692d40f7e89c04fabf4a3077e61
Naoto Takai <takai@recompile.net>
2016-06-24 12:03:23 +0900

    Use single quote

--- lib/flickr_to_google_photos.rb
+++ lib/flickr_to_google_photos.rb
@@ -10,11 +10,11 @@ require 'dotenv'
 
 Dotenv.load
 
-require "flickr_to_google_photos/version"
-require "flickr_to_google_photos/downloader"
+require 'flickr_to_google_photos/version'
+require 'flickr_to_google_photos/downloader'
 
-require "flickr_to_google_photos/api/flickr"
-require "flickr_to_google_photos/api/google_photos"
+require 'flickr_to_google_photos/api/flickr'
+require 'flickr_to_google_photos/api/google_photos'
 
 require 'flickr_to_google_photos/model/album'
 require 'flickr_to_google_photos/model/photo'
