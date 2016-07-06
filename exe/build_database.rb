ed203c1b485e315b788ba40f3919f6b8609f9efc
Naoto Takai <takai@recompile.net>
2016-07-07 06:25:10 +0900

    Add script to biuld database

---
+++ exe/build_database.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby -I lib
+
+require 'bundler'
+Bundler.require(:default)
+
+require_relative '../lib/flickr_to_google_photos'
+
+flickr = FlickrToGooglePhotos::Flickr.new(ENV['FLICKR_CONSUMER_KEY'],
+                                          ENV['FLICKR_CONSUMER_SECRET'],
+                                          ENV['FLICKR_ACCESS_TOKEN'],
+                                          ENV['FLICKR_ACCESS_TOKEN_SECRET'])
+albums = flickr.each_albums
+albums.map(&:save)
+albums.each do |album|
+  flickr.each_photos_by_album(album).map(&:save)
+end
+
+flickr.each_photos_not_in_set.map(&:save)
