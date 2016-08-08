f9b55665b5ea50cff2d70fc80548657bc70c9282
Naoto Takai <takai@recompile.net>
2016-08-09 07:08:50 +0900

    Handle error

--- exe/upload_photos.rb
+++ exe/upload_photos.rb
@@ -5,16 +5,26 @@ Bundler.require(:default)
 
 require_relative '../lib/flickr_to_google_photos'
 
-photos = FlickrToGooglePhotos::GooglePhotos.new(ENV['GOOGLE_USER_ID'],
-                                                ENV['GOOGLE_CLIENT_ID'],
-                                                ENV['GOOGLE_CLIENT_SECRET'],
-                                                ENV['GOOGLE_REFRESH_TOKEN'])
+def new_photos
+  FlickrToGooglePhotos::GooglePhotos.new(ENV['GOOGLE_USER_ID'],
+                                         ENV['GOOGLE_CLIENT_ID'],
+                                         ENV['GOOGLE_CLIENT_SECRET'],
+                                         ENV['GOOGLE_REFRESH_TOKEN'])
+end
+
+photos = new_photos
 
 puts "Start uploading..."
 FlickrToGooglePhotos::Model::Photo.connection
 FlickrToGooglePhotos::Model::Photo.where(uploaded_at: 0).each do |photo|
-  puts "Fetching #{photo.url}..."
-  binary = HTTPClient.new.get(photo.url).body
-  puts "Uploading #{photo.url}..."
-  photos.upload_photo(photo.album || default_album, photo, binary)
+  begin
+    puts "Fetching #{photo.url}..."
+    binary = HTTPClient.new.get(photo.url).body
+    puts "Uploading #{photo.url}..."
+    photos.upload_photo(photo.album || default_album, photo, binary)
+  rescue Picasa::ForbiddenError => e
+    photos = new_photos
+  rescue Picasa::BadRequestError, Encoding::CompatibilityError => e
+    warn "[WARN] #{photo.url}: #{e.message}"
+  end
 end
